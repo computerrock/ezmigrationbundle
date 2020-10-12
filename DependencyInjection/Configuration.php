c8dd62b33bdd3a522ecc094c463c0ce7ec4a9560
Aleksandar Jakovljevic <aleksandar.jakovljevic@computerrock.com>
2020-10-12 16:30:27 +0200

    Fix config deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ez_migration_bundle');
+        $treeBuilder = new TreeBuilder('ez_migration_bundle');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('ez_migration_bundle');
+        }
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
